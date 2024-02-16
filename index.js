9793c53cd48f10099d9ffb4cc39d75ba8db3803f
Bruno Samuel <brunosag02@gmail.com>
2024-02-16 02:04:56 -0300

    feat: proccess and format professores and horarios

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const user = process.env.PORTAL_USER;
 const password = process.env.PORTAL_PASSWORD;
 
 async function signIn(page) {
+  if (user === undefined || password === undefined) {
+    throw new Error('Please set PORTAL_USER and PORTAL_PASSWORD environment variables');
+  }
+
   await page.type('#usuario', user);
   await page.type('#senha', password);
   await page.click('input[name="login"]', { waitUntil: 'networkidle0' });
@@ -63,30 +67,53 @@ app.get('/:id', async (req, res) => {
   // get information from table
   const table = await page.$('#Horarios tbody');
   const rowElements = await table.$$('tr');
-  const rows = [];
-  for (const rowElement of rowElements) {
-    const tds = await rowElement.$$eval('td', (tds) => tds.map((td) => td.textContent.trim()));
-    rows.push(tds);
-  }
-
-  // assemble cadeiras' objects
   const cadeiras = [];
   let cadeira;
-  for (const row of rows.slice(1)) {
+  for (const rowElement of rowElements.slice(1)) {
+    const tds = await rowElement.$$('td');
+
+    // proccess horarios
+    let horarios = await tds[8].$$eval('li', (items) => items.map((item) => item.textContent.trim()));
+    if (horarios[0] === 'Horário não definido.') {
+      horarios = null;
+    } else {
+      horarios = horarios.map((item) => {
+        const [dia, horario] = item.split(' ');
+        return { dia, horario };
+      });
+    }
+
+    // proccess professores
+    let professores = await tds[9].$$eval('li', (items) => items.map((item) => item.textContent.trim()));
+    if (professores[0] === 'Professor não definido.') {
+      professores = null;
+    } else {
+      professores = professores.map((item) => {
+        return {
+          nome: item.split('-')[0].trim(),
+          regente: item.includes('Regente'),
+          ministrante: item.includes('Ministrante'),
+          responsavel_conceito: item.includes('Responsável conceito'),
+        };
+      });
+    }
+
+    // assemble cadeira and turma
     const turma = {
-      turma: row[2],
-      vagas_veteranos: row[3],
-      vagas_calouros: row[4],
-      horarios: row[8] === 'Horário não definido.' ? null : row[8].replace(/\"|2\\n/g, '').split(' '),
-      professores: row[9] === 'Professor não definido.' ? null : row[9].split(' '),
+      turma: (await (await tds[2].getProperty('textContent')).jsonValue()).trim(),
+      vagas_veteranos: parseInt((await (await tds[3].getProperty('textContent')).jsonValue()).trim(), 10),
+      vagas_calouros: parseInt((await (await tds[4].getProperty('textContent')).jsonValue()).trim(), 10),
+      horarios,
+      professores,
     };
-    if (row[0]) {
+    const cadeiraName = (await (await tds[0].getProperty('textContent')).jsonValue()).trim();
+    if (cadeiraName) {
       if (cadeira) {
         cadeiras.push(cadeira);
       }
       cadeira = {
-        name: row[0],
-        creditos: row[1],
+        name: cadeiraName,
+        creditos: parseInt((await (await tds[1].getProperty('textContent')).jsonValue()).trim(), 10),
         turmas: [turma],
       };
     } else {
