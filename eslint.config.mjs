6530539da61f1f82e85df4c9f0a0a1afa7f814f3
brunosag <brunosag02@gmail.com>
2024-09-02 16:24:19 -0300

    chore: set up typescript, eslint & prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import eslint from "@eslint/js";
+import prettierConfig from "eslint-config-prettier";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+	eslint.configs.recommended,
+	...tseslint.configs.strictTypeChecked,
+	...tseslint.configs.stylisticTypeChecked,
+	{
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+	},
+	{
+		files: ["**/*.mjs"],
+		extends: [tseslint.configs.disableTypeChecked],
+	},
+	prettierConfig,
+);
